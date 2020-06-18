76e547f93264786d5515e2f3f601cd60159d3b3b
Vilson Borges <vilsonborges98@gmail.com>
2020-06-18 19:36:17 -0300

    listagem dos agendamentos pelos usuarios e prestadores

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddlewere from './app/middleweres/auth';
 import AppointmentController from './app/controllers/AppointmentController';
@@ -23,7 +24,10 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
+
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
