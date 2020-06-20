2d70a31a7ca6755e98d573c4d98b814775429723
Vilson Borges <vilsonborges98@gmail.com>
2020-06-20 17:27:14 -0300

    Listagem de horarios disponiveis de um prestador

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddlewere from './app/middleweres/auth';
 import AppointmentController from './app/controllers/AppointmentController';
@@ -23,6 +24,7 @@ routes.use(authMiddlewere);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.post('/appointments', AppointmentController.store);
 
