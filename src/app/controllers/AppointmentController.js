34d6a30407dd0d47663cc7f4ad9cc151e7c95c08
Vilson Borges <vilsonborges98@gmail.com>
2020-06-20 09:20:52 -0300

    templates de email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -128,10 +128,13 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
-
-    console.log(appointment.provider.email);
 
     if (appointment.user_id !== req.userId) {
       return res.status(401).json({
@@ -155,7 +158,15 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento', // Could be html
+      template: 'cancellation',
+      context: {
+        // the variables that are expected
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
